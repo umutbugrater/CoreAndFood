3155a4e9c39cfbd012220c57c591aef37e6a7b17
Umut Buğra TER <umutbugra.ter.9@hotmail.com>
2024-01-14 22:06:20 +0300

    update

--- CoreAndFood-seed/CoreAndFood/Controllers/ChartController.cs
+++ CoreAndFood-seed/CoreAndFood/Controllers/ChartController.cs
@@ -55,7 +55,7 @@ namespace CoreAndFood.Controllers
         public List<Class2> FoodList()
         {
             List<Class2> cs2 = new List<Class2>();
-            #region Kullanım-1 hocanın yaptığı
+
             using (var c = new Context())
             {
                 cs2 = c.Foods.Select(x => new Class2
@@ -64,26 +64,6 @@ namespace CoreAndFood.Controllers
                     stock = x.Stock
                 }).ToList();
             }
-            #endregion
-
-            #region Kullanım2
-            //Context c = new Context();
-            //cs2 = c.Foods.Select(x => new Class2
-            //{
-            //    foodname = x.Name,
-            //    stock = x.Stock
-            //}).ToList();
-            #endregion
-
-            #region Kullanım3
-            //FoodRepository f = new FoodRepository();
-
-            //cs2 = f.TList().Select(x => new Class2
-            //{
-            //    foodname = x.Name,
-            //    stock = x.Stock
-            //}).ToList();
-            #endregion
 
 
             return cs2;
@@ -97,19 +77,19 @@ namespace CoreAndFood.Controllers
             var deger2 = c.Categories.Count();
             ViewBag.d2 = deger2;
 
-            var foid = c.Categories.Where(x => x.CategoryName == "Fruit").Select(y => y.CategoryID).FirstOrDefault();
+            var foid = c.Categories.Where(x => x.CategoryName == "Meyveler").Select(y => y.CategoryID).FirstOrDefault();
             //ViewBag.d=foid;
             var deger3 = c.Foods.Where(x => x.CategoryID == foid).Count();
             //var deger3 = c.Foods.Where(x => x.Category.CategoryID == 1).Count();
             ViewBag.d3 = deger3;
 
-            var deger4 = c.Foods.Where(x => x.CategoryID == (c.Categories.Where(x => x.CategoryName == "Vegetables").Select(y => y.CategoryID).FirstOrDefault())).Count();
+            var deger4 = c.Foods.Where(x => x.CategoryID == (c.Categories.Where(x => x.CategoryName == "Sebzeler").Select(y => y.CategoryID).FirstOrDefault())).Count();
             ViewBag.d4 = deger4;
 
             var deger5 = c.Foods.Sum(x => x.Stock);
             ViewBag.d5 = deger5;
 
-            var deger6 = c.Foods.Where(x => x.CategoryID == c.Categories.Where(y => y.CategoryName == "Legumes").Select(z => z.CategoryID).FirstOrDefault()).Count();
+            var deger6 = c.Foods.Where(x => x.CategoryID == c.Categories.Where(y => y.CategoryName == "Bakliyat").Select(z => z.CategoryID).FirstOrDefault()).Count();
             ViewBag.d6 = deger6;
 
             //descending = tersten sıralama z den a ya doğru -> büyükten küçüğe
@@ -123,11 +103,11 @@ namespace CoreAndFood.Controllers
             var deger9 = c.Foods.Average(x => x.Price).ToString("0.00");
             ViewBag.d9 = deger9;
 
-            var deger10 = c.Categories.Where(x => x.CategoryName == "Fruit").Select(y => y.CategoryID).FirstOrDefault();
+            var deger10 = c.Categories.Where(x => x.CategoryName == "Meyveler").Select(y => y.CategoryID).FirstOrDefault();
             var deger10p = c.Foods.Where(y => y.CategoryID == deger10).Sum(x => x.Stock);
             ViewBag.d10 = deger10p;
 
-            var deger11 = c.Categories.Where(x => x.CategoryName == "Vegetables").Select(y => y.CategoryID).FirstOrDefault();
+            var deger11 = c.Categories.Where(x => x.CategoryName == "Sebzeler").Select(y => y.CategoryID).FirstOrDefault();
             var deger11p = c.Foods.Where(y => y.CategoryID == deger11).Sum(x => x.Stock);
             ViewBag.d11 = deger11p;
 
